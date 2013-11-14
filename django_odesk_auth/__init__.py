f8b186b3d5d9530eea7e1d7480219ea8e943c41e
Anton Strogonoff <anton@strogonoff.name>
2013-11-14 05:35:08 +0200

    Documented O_REQUEST_TOKEN and O_ACCESS_TOKEN constants.

--- django_odesk_auth/__init__.py
+++ django_odesk_auth/__init__.py
@@ -3,4 +3,9 @@
 """Offers "Log in via oDesk" functionality."""
 
 O_REQUEST_TOKEN = '_ort'
+"""Session key under which OAuth request token will be stored
+during authentication."""
+
 O_ACCESS_TOKEN = '_oat'
+"""Session key under which OAuth access token will be stored
+once authentication is complete."""
