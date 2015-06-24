0c9b87539da08e917d022fdd69219c6bc600517c
Anton Strogonoff <anton@strogonoff.name>
2015-06-24 20:41:14 +0300

    Bumping version from pre-release to actual 0.2

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-odesk-auth',
-    version='0.2-pre2',
+    version='0.2',
     packages=['django_odesk_auth'],
     include_package_data=True,
     license='BSD License',
